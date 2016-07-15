17e8a957d79c96531f8bbbd6228fd6b02fa03509
keetz <heykeetz@gmail.com>
2016-07-15 18:40:21 +0530

    After the release

--- src/test/java/org/wso2/carbon/connector/integration/test/confluence/ConfluenceConnectorIntegrationTest.java
+++ src/test/java/org/wso2/carbon/connector/integration/test/confluence/ConfluenceConnectorIntegrationTest.java
@@ -45,7 +45,7 @@ public class ConfluenceConnectorIntegrationTest extends ConnectorIntegrationTest
     @BeforeClass(alwaysRun = true)
     public void setEnvironment() throws Exception {
 
-        init("confluence-connector-2.0.1");
+        init("confluence-connector-2.0.2-SNAPSHOT");
 
         esbRequestHeadersMap.put("Content-Type", "application/json");
 
